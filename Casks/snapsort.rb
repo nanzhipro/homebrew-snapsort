34df17cc1b003b354bafd2f0e570cefbee32302c
南知 <deer_hope@live.cn>
2025-06-09 09:09:22 +0800

    Add app cask v1.0.0

---
+++ Casks/snapsort.rb
@@ -0,0 +1,9 @@
+cask "snapsort" do
+    version "1.0.0"
+    sha256 "a1fcfe06a8df8c7881b74b726beb376855dabd58fa56002893910794ef675c8c"
+    url "https://github.com/nanzhipro/SnapSort/releases/download/v1.0/SnapSort-1.2.dmg"
+    name "SnapSort"
+    desc "SnapSort is a tool that helps you sort your snaps."
+    homepage "https://github.com/nanzhipro/SnapSort"
+    app "SnapSort.app"
+  end
