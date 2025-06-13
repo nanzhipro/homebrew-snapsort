34d691ae21ef1193575022619ace593906db02c5
南朋友 <deer_hope@live.cn>
2025-06-13 08:24:35 +0800

    Update snapsort.rb

--- Casks/snapsort.rb
+++ Casks/snapsort.rb
@@ -1,7 +1,7 @@
 cask "snapsort" do
-    version "1.0.1"
-    sha256 "609f2196ff461da9a5f2f4414a9e79970f072e6bb41f77cb07108dcf82a30c79"
-    url "https://github.com/nanzhipro/SnapSort/releases/download/v1.0.1/SnapSort-1.0.1.dmg"
+    version "1.0.2"
+    sha256 "c871f08e7fc9ce9d040334d35f1ba5d77b52ed433f8fb00573af57732f6d71d4"
+    url "https://github.com/nanzhipro/SnapSort/releases/download/v1.0.2/SnapSort-1.0.2.dmg"
     name "SnapSort"
     desc "AI-powered screenshot organization app for macOS."
     homepage "https://github.com/nanzhipro/SnapSort"
